6975c0689b88a9c709fc39c3f462fe0a09ad9a91
Leopoldo013 <glaucioleopoldo@gmail.com>
2021-06-12 23:45:17 -0300

    prmeiro commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import time
+from turtle import Screen, Turtle
+from food import Food
+from scoreboard import Scoreboard
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=800, height=800)
+screen.bgcolor("#301b3f")
+screen.title("Crazy-Snake")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 380 or snake.head.xcor() < -380 or snake.head.ycor() > 380 or snake.head.ycor() < -380:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
